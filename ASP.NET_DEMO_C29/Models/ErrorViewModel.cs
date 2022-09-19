487b274e9c595f148894c269e7a33c5985fea18d
Eddie Z <e.zarate7@hotmail.com>
2022-09-19 09:19:15 -0600

    deleted appsettings.json

---
+++ ASP.NET_DEMO_C29/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_DEMO_C29.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
