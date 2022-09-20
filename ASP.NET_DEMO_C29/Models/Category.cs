6286f52d28ad19fdd18516b2cd8dad6a0555bddf
ezarate7 <ezarate7@outlook.com>
2022-09-19 18:04:21 -0600

    update

---
+++ ASP.NET_DEMO_C29/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_DEMO_C29.Models
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
